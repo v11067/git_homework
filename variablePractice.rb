191cea0344b71f6bfb330697138b005faee75050
Kohei Sadahiro <koheismacbookair@Koheis-MacBook-Air.local>
2021-02-23 13:15:05 -0700

    change 3variable

--- variablePractice.rb
+++ variablePractice.rb
@@ -1,6 +1,6 @@
 var i_am_a_string = "Hello World";
-var number_of_cats = 1;
-var pregnant_cats = 1.5;
+var number_of_cats = 8;
+var pregnant_cats = 4.5;
 var pizza_sause = true;
 var pizza_sause = false;
-var soda_inventry = ["Coke", "Pepsi", "Remonado"]
+var soda_inventry = ["Coke", "Orange", "Remonado"]
